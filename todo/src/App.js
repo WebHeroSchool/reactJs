1f8175f487da77f4ad153dc35dc21d606fbfa51e
Ayana <ayana.sultanovaa@gmail/com>
2021-01-27 19:28:00 +0600

    Был создан элемент в файле app.js и передан в метод ReactDOM.render() в index.js, чтобы он "отрендерился" в нашу разметку.

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,55 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import { count, length } from './numbers';
-const a = 'App';
-const num = 7;
-const num1 = 6;
-const app = false;
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-           {count * length}
-        </p> 
-        <h3> 
-          {num + num1}
-        </h3>
-        <p style={{
-          color: 'blue'
-        }}>
-          Hello world!
-        </p>
-        <p>
-          {a}
-        </p>
-        <p>
-          {app || 'App is true'}
-        </p>
-        <p>
-          {app ? 'App is true' : 'App is false'}
-        </p>
-        <p>
-          {undefined}
-          {null}
-          {true}
-          {false}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
 
-export default App;
+const element = (<div>
+  Hello world!
+  <a href="">go to</a>
+</div>)
+
+export {element};
