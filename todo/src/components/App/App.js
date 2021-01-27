eb871e150ea6b85865690438a5dc58e21f48953a
Ayana <ayana.sultanovaa@gmail/com>
2021-01-27 22:05:39 +0600

    1. Было передано в Footer количество дел, которые еще нужно выполнить.
    2. Переданы три разных значения в Item.

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
-const App = () => (<div> 
-    <ItemList />
+import Footer from '../Footer/Footer';
+
+const toDoItem = 'Написать новое приложение';
+const App = () => (<div>
+    <h1>Важные дела:</h1> 
+    <InputItem />
+    <ItemList todoItem = {toDoItem} />
+    <Footer count={3} />
 </div>)
 
 export default App;
--- todo/src/components/ItemList/ItemList.js
+++ todo/src/components/ItemList/ItemList.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Item from '../Item/Items'
-const ItemList = () => (<ul>
-   <li> <Item /> </li>
-   <li> <Item /> </li>
-   <li> <Item /> </li>
+import Item from '../Item/Items';
+
+const ItemList = ({ todoItem }) => (<ul>
+   <li> <Item todoItem={todoItem} /> </li>
+   <li> <Item todoItem={'прописать props-ы'} /> </li>
+   <li> <Item todoItem={'отправить pullrequest'} /> </li>
 </ul>);
 
 export default ItemList; 
