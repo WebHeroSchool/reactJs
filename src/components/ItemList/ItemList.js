81b2de4f3973aef08ec3868b1f44debf6fc12d4c
DESKTOP-U6O47B3\User <ayana.sultanovaa@gmail.com>
2021-02-22 16:14:48 +0300

    no message

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import Item from '../Item/Items';
+import Checkbox from '@material-ui/icons/Checkbox';
+
+const Check = () => <div>
+    <Checkbox
+        control={<Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} name="checkedH" />}
+        label="Custom icon"
+      />
+</div>
 
 const ItemList = ({ items }) => (<ul>
    {items.map(item => <li key={item.value}><Item value={item.value} isDone={item.isDone} /></li>)}
